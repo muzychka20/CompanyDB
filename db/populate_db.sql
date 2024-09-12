5266e04f23c25f4604a74c5bcd76c25064a5a8d9
KyryloMuzychka <kyryll2016@gmail.com>
2024-09-12 18:06:35 +0300

    feat: populate database

---
+++ db/populate_db.sql
@@ -0,0 +1,8 @@
+USE CompanyDB;
+
+INSERT INTO Countries(CountryName) VALUES ('Ukraine');
+INSERT INTO Countries(CountryName) VALUES ('Germany');
+
+INSERT INTO Cities(CityName, CountryId) VALUES ('Kyiv', 16);
+INSERT INTO Cities(CityName, CountryId) VALUES ('Dnipro', 16);
+INSERT INTO Cities(CityName, CountryId) VALUES ('Berlin', 17);
